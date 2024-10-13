59d892fb6db40db56d98bec9d2f9644df7a0bfb0
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2024-10-13 18:18:06 +0600

    done

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,9 +1,9 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: "live",
-  client_id: "",
-  client_secret: "",
+  mode: "sandbox",
+  client_id: "AZWzZ2TZfuOneCKSzKxqLKwCdIqt0qOD88TX7OxK7AP0vZ7c4l9eNS2c8THHS6OC4GDA2-wh1Kco57Px",
+  client_secret: "EIBRxLPng0NS_t0JzbBVO4e69XeTG-zMZddKrTqs9ocbEOL-_5x2k86u6L-VStul5SCih7q4XOQtvYTW",
 });
 
 module.exports = paypal;
